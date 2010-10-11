2b628d2d214d1de534474334e76da8bb478bb5ad
Kasper Meerts <kaspermeerts@gmail.com>
2010-10-11 17:22:37 +0200

    More vector methods

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "Particle.hpp"
 
 vec3::vec3(float a, float b, float c)
@@ -7,7 +8,7 @@ vec3::vec3(float a, float b, float c)
 	z = c;
 }
 	
-vec3 vec3::operator+(const vec3 &v)
+vec3 vec3::operator+(const vec3 &v) const
 {
 	float a, b, c;
 
@@ -16,4 +17,30 @@ vec3 vec3::operator+(const vec3 &v)
 	c = z + v.z;
 
 	return vec3(a, b, c);
+}
+
+vec3 vec3::operator-(const vec3 &v) const
+{
+	const vec3 that = v * (-1);
+	return that + (*this);
+}
+
+float vec3::operator*(const vec3 &v) const
+{
+	return x*v.x + y*v.y + z*v.z;
+}
+
+vec3 vec3::operator*(float v) const
+{
+	return vec3(x*v, y*v, z*v);
+}
+
+vec3 vec3::operator/(float v) const
+{
+	return (*this * (1/v));
+}
+
+float vec3::length() const
+{
+	return sqrt(x*x + y*y + z*z);
 }
--- src/Particle.hpp
+++ src/Particle.hpp
@@ -4,8 +4,13 @@
 struct vec3
 {
 	float x, y, z;
-	vec3 operator+(const struct vec3 &v);
 	vec3(float a, float b, float c);
+	vec3 operator+(const vec3 &v) const;
+	vec3 operator-(const vec3 &v) const;
+	float operator*(const vec3 &v) const;
+	vec3 operator*(float v) const;
+	vec3 operator/(float v) const;
+	float length(void) const;
 };
 
 #endif
