11d6bce5494c0c043348d01143e8411616774f62
Roald Frederickx <roald.frederickx@gmail.com>
2010-11-01 13:58:04 +0100

    fixed directory in makeGraph, added two performance images as test

--- src/scripts/octave/makeGraph.m
+++ src/scripts/octave/makeGraph.m
@@ -1,9 +1,10 @@
-function makeGraph(name,destdir,xlab,ylab,ylabrule,width,height);
+function makeGraph(name,destdir,relImgDir,xlab,ylab,ylabrule,width,height);
 	xlabel(xlab);
 	ylabel(['\rule{0pt}{',ylabrule,'}',ylab]);
 
 	system(['mkdir ',destdir,' &>/dev/null']);
 	print([destdir,'/',name,'.tex'],'-depslatex',['-S',width,',',height]);
 	system(['cd ',destdir,'; epstopdf ',name,'.eps; rm ',name,'.eps']);
+	system(['cd ',destdir,"; sed -e 's#",destdir,'#',relImgDir,"#' ", name,".tex > ",name,"-fixed.tex; mv ",name,"-fixed.tex ", name,".tex"]);
 endfunction
 
